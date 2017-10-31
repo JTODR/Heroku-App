5afc9bd8e47f69c9df736116d71017d3f0315bf3
Joseph <josephodr@hotmail.com>
2017-10-31 19:38:52 +0000

    Changed function name

--- my_app.py
+++ my_app.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "<h1>Hello MasterCard</h1><h3>Joseph O'Donovan</h3>"
+@app.route('/')     #Tell flask what URL should trigger the function
+def hello_mastercard():
+    heading = "<h1>Hello MasterCard</h1>"
+    subheading = "<h3>Joseph O'Donovan</h3>"
+    return heading + subheading
