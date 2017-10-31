08018c726fe1f14eb8516a564188100c3882d9f7
Joseph <josephodr@hotmail.com>
2017-10-31 19:34:21 +0000

    Typo

--- my_app.py
+++ my_app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello MasterCard</h1><h3>Joseph O'Donovan<\h3>"
+    return "<h1>Hello MasterCard</h1><h3>Joseph O'Donovan</h3>"
